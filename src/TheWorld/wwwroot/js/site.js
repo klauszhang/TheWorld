555283c9e43e989a0ac541297a6d4bef27806054
Hao Zhang <klaushaozhang.note@gmail.com>
2015-11-03 10:51:08 +1300

    add bootstrap and font-awesome

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,14 +1,18 @@
 (function () {
 
   var $sidebarAndWarpper = $("#sidebar, #warpper");
+  var $icon = $("#sidebarToggle i.fa");
 
   $("#sidebarToggle").on("click", function () {
     $sidebarAndWarpper.toggleClass("hide-sidebar");
     if ($sidebarAndWarpper.hasClass("hide-sidebar")) {
-      $(this).text("Show Sidebar");
+      $icon.removeClass("fa-angle-left");
+      $icon.addClass("fa-angle-right");
+
     }
     else {
-      $(this).text("Hide Sidebar");
+      $icon.removeClass("fa-angle-right");
+      $icon.addClass("fa-angle-left");
     }
   });
 
