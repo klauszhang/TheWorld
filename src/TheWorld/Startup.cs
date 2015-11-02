764b086af57a9ee2df1bbc1c65c36b7f714e9430
Hao Zhang <klaushaozhang.note@gmail.com>
2015-11-02 18:00:29 +1300

    add route of first controller

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -8,24 +8,34 @@ using Microsoft.Framework.DependencyInjection;
 
 namespace TheWorld
 {
-    public class Startup
+  public class Startup
+  {
+    // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
       services.AddMvc();
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-      app.UseStaticFiles();
-            // Add the platform handler to the request pipeline.
-            //app.UseIISPlatformHandler();
-
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
-        }
     }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseStaticFiles();
+
+      app.UseMvc(config=>
+      {
+        config.MapRoute(
+            name: "Default",
+            template: "{controller}/{action}/{id?}",
+            defaults: new { controller = "App", action = "Index" }
+            );
+      });
+
+      // Add the platform handler to the request pipeline.
+      //app.UseIISPlatformHandler();
+
+      //app.Run(async (context) =>
+      //{
+      //    await context.Response.WriteAsync("Hello World!");
+      //});
+    }
+  }
 }
