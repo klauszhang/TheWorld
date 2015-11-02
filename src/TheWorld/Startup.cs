961c175293624d6178b0f4cf02b06d5deb35816b
Hao Zhang <klaushaozhang.note@gmail.com>
2015-11-03 00:49:51 +1300

    add server side validation and configuration

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -7,31 +7,46 @@ using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
 using TheWorld.Services;
 using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.Configuration;
+using Microsoft.Dnx.Runtime;
 
 namespace TheWorld
 {
   public class Startup
   {
+    public static IConfigurationRoot Configuration;
+    public Startup(IApplicationEnvironment appEnv)
+    {
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(appEnv.ApplicationBasePath)
+        .AddJsonFile("config.json")
+        .AddEnvironmentVariables();
+
+      Configuration = builder.Build();
+
+    }
+
     // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services,IHostingEnvironment env)
+    public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
 
-      if (env.IsDevelopment())
-      {
-        services.AddScoped<IMailService, DebugMailServices>();
-      }
-      else
-      {
-        // services.AddScoped<IMailService, RealMailServices>();
-      }
+      // if (env.IsDevelopment())
+      // IHostingEnvironment env
+      // {
+      services.AddScoped<IMailService, DebugMailServices>();
+      // }
+      //else
+      //{
+      //  // services.AddScoped<IMailService, RealMailServices>();
+      //}
     }
 
     public void Configure(IApplicationBuilder app)
     {
       app.UseStaticFiles();
 
-      app.UseMvc(config=>
+      app.UseMvc(config =>
       {
         config.MapRoute(
             name: "Default",
