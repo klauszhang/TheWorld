09a46a65a71df040a60e6206a16e9d8fef1c39f9
Hao Zhang <klaushaozhang.note@gmail.com>
2015-11-02 17:40:05 +1300

    add js and css

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,5 +1,16 @@
 (function () {
 
-  
+  var $sidebarAndWarpper = $("#sidebar, #warpper");
+
+  $("#sidebarToggle").on("click", function () {
+    $sidebarAndWarpper.toggleClass("hide-sidebar");
+    if ($sidebarAndWarpper.hasClass("hide-sidebar")) {
+      $(this).text("Show Sidebar");
+    }
+    else {
+      $(this).text("Hide Sidebar");
+    }
+  });
+
 
 })();
