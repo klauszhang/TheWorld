26d4b79cfcd85776b0d68dfda1eb771f75043cc5
Hao Zhang <klaushaozhang.note@gmail.com>
2015-11-02 17:51:04 +1300

    add mvc support

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -13,6 +13,7 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+      services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
