64c928738888c42b331e9ba4a54342bdfed9053b
Hao Zhang <klaushaozhang.note@gmail.com>
2015-11-03 11:59:23 +1300

    add connections tring

--- src/TheWorld/Models/WorldContext.cs
+++ src/TheWorld/Models/WorldContext.cs
@@ -10,5 +10,15 @@ namespace TheWorld.Models
   {
     public DbSet<Stop> Stops { get; set; }
     public DbSet<Trip> Trips { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      var connString = Startup.Configuration["Data:WorldContextConnection"];
+      optionsBuilder.UseSqlServer(connString);
+
+
+      base.OnConfiguring(optionsBuilder);
+    }
+
   }
 }
