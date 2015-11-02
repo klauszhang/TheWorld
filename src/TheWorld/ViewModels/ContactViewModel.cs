df48fa681f08241d0768250fdd7f5d1756bed7c3
Hao Zhang <klaushaozhang.note@gmail.com>
2015-11-02 23:56:09 +1300

    Add ViewModel to contact

---
+++ src/TheWorld/ViewModels/ContactViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+namespace TheWorld.ViewModels
+{
+  public class ContactViewModel
+  {
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Message { get; set; }
+  }
+}
