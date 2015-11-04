327548ea084338fc6bfc45e9233c4b19701eb520
Hao Zhang <klaushaozhang.note@gmail.com>
2015-11-04 22:15:34 +1300

    fixed

--- src/TheWorld/Models/WorldContext.cs
+++ src/TheWorld/Models/WorldContext.cs
@@ -5,7 +5,21 @@ namespace TheWorld.Models
 {
   public class WorldContext:DbContext
   {
+    public WorldContext()
+    {
+      Database.EnsureCreated();
+    }
+
     public DbSet<Trip> Trips { get; set; }
     public DbSet<Stop> Stops { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      string connString = Startup.Configuration["Data:WorldContextConnection"];
+
+      optionsBuilder.UseSqlServer(connString);
+
+      base.OnConfiguring(optionsBuilder);
+    }
   }
 }
